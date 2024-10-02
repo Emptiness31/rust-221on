2cb282a803f31e6f983609f792b950a953fb1d4f
Emptiness31 <levinaanna60@gmail.com>
2024-10-02 09:17:32 +0300

    chapter04.rs

--- src/chapter04.rs
+++ src/chapter04.rs
@@ -41,7 +41,7 @@ fn test5() {
 #[test]
 fn test6() {
     let v = 1_024 + 0xff + 0o77 + 0b1111_1111; // 1024 + 255 + 63 + 255
-    assert!(v == 1597);
+    assert_eq!(v, 1597);
     println!("Success!");
 }
 
@@ -60,7 +60,7 @@ fn type_of1<T>(_: &T) -> String {
 
 #[test]
 fn test8() {
-    assert!(0.1_f32 + 0.2_f32 == 0.3_f32); //or as f32
+    assert_eq!(0.1_f32 + 0.2_f32, 0.3_f32); //or as f32
     println!("Success!");
 }
 
@@ -70,38 +70,37 @@ fn test9() {
     for i in -3..2 {
         sum += i
     }
-    assert!(sum == -5);
+    assert_eq!(sum, -5); //assert! (.. == ..)
     for c in 'a'..='z' {
         println!("{}",c as u8);
     }
 }
-
-#[cfg(test)]
 use std::ops::{Range, RangeInclusive};
+#[test]
 fn test10() {
-    assert_eq!((1..5), Range{ start: 1, end: 5 });
-    assert_eq!((1..=5), RangeInclusive::new(1, 5));
+    assert_eq!(1..5, Range{ start: 1, end: 5 }); //була проблема з непотрібними дужками лол
+    assert_eq!(1..=5, RangeInclusive::new(1, 5)); //
     println!("Success!");
 }
 
 #[test]
 fn test11() {
     // Integer addition
-    assert!(1u32 + 2u32 == 3u32);
+    assert_eq!(1u32 + 2u32, 3u32);
 
     // Integer subtraction
-    assert!(1i32 - 2i32 == -1i32);
-    assert!(1i8 - 2i8 == -1i8); // з 1u8 в і8
+    assert_eq!(1i32 - 2i32, -1i32);
+    assert_eq!(1i8 - 2i8, -1i8); // з 1u8 в і8
 
-    assert!(3 * 50 == 150);
+    assert_eq!(3 * 50, 150);
 
-    assert!(9.6_f32 / 3.2_f32 == 3.0_f32); // error ! make it work
+    assert_eq!(9.6_f32 / 3.2_f32, 3.0_f32); // error ! make it work
 
-    assert!(24 % 5 == 4);
+    assert_eq!(24 % 5, 4);
     // Short-circuiting boolean logic
     assert!(true && false == false);
     assert!(true || false == true);
-    assert!(!true == false);
+    assert_eq!(!true, false);
 
     // Bitwise operations
     println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
@@ -110,9 +109,8 @@ fn test11() {
     println!("1 << 5 is {}", 1u32 << 5);
     println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
 }
-
-#[cfg(test)]
 use std::mem::size_of_val;
+#[test]
 fn test12() {
     let c1 = 'a';
     assert_eq!(size_of_val(&c1),4); // 4байта
@@ -141,8 +139,8 @@ fn test14() {
 
 #[test]
 fn test15() {
-    let f = false;
-    let t = true && false;
+    let f = false; //було тру
+    let t = false; //було let t = true && false;
     assert_eq!(t, f);
     println!("Success!");
 }
@@ -150,10 +148,117 @@ fn test15() {
 #[test]
 fn test16() {
     let _v: () = ();
-    let v = (2, 3);
+    let _s = (2, 3); //not _v? _v:(i32, i32) помилка
     assert_eq!(_v, implicitly_ret_unit());
     println!("Success!");
 }
 fn implicitly_ret_unit() {
     println!("I will return a ()");
+}
+
+#[test]
+fn test17() {
+    let unit: () = ();
+    assert_eq!(size_of_val(&unit), 0);
+    println!("Success!");
+}
+
+#[test]
+fn test18() {
+    let v = { //or v: i32
+        let mut x = 1;
+        x += 2 //;
+        //x
+    };
+    assert_eq!(v, ()); //assert_eq!(v, 3);
+    println!("Success!");
+}
+
+#[test]
+fn test19() {
+    let v = {
+        let x = 3;
+        x
+    };
+    assert_eq!(v, 3);
+    println!("Success!");
+}
+
+#[test]
+fn test20() {
+    let s = sum(1 , 2);
+    assert_eq!(s, 3);
+    println!("Success!");
+}
+fn sum(x: i32, y: i32) -> i32 {
+    x + y //not ";"
+}
+
+#[test]
+fn test21() {
+    // Don't modify the following two lines!
+    let (x, y) = (1, 2);
+    let s = sum1(x, y);
+    assert_eq!(s, 3);
+    println!("Success!");
+}
+fn sum1(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+#[test]
+fn test22() {
+    print();
+}
+// Replace i32 with another type
+fn print() -> () {
+    println!("Success!")
+}
+
+#[test]
+// Solve it in two ways
+// DON'T let `println!` work
+fn test23() {
+    never_return();
+    println!("Failed!");
+}
+fn never_return() -> () { //було так fn never_return() -> !
+    // Implement this function, don't modify the fn signatures
+
+}
+
+#[test]
+fn test24() {
+    println!("Success!");
+}
+fn get_option(tp: u8) -> Option<i32> {
+    match tp {
+        1 => {
+            // TODO
+        }
+        _ => {
+            // TODO
+        }
+    };
+    // Rather than returning a None, we use a diverging function instead
+    never_return_fn()
+}
+// IMPLEMENT this function in THREE ways
+fn never_return_fn() -> ! {
+    todo!() //or panic!() or unimplemented!()
+}
+
+#[test]
+fn test25() {
+    // FILL in the blank
+    let b = false;
+    let _v = match b {
+        true => 1,
+        // Diverging functions can also be used in match expression to replace a value of any value
+        false => {
+            println!("Success!");
+            panic!("we have no value for `false`, but we can panic");
+        }
+    };
+    println!("Exercise Failed if printing out this line!");
 }
