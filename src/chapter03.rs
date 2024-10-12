bc263fe26ce3daccbf1554f0f88e25a7de6f176c
Emptiness31 <levinaanna60@gmail.com>
2024-10-12 18:45:01 +0300

    chapter03.rs

--- src/chapter03.rs
+++ src/chapter03.rs
@@ -1,20 +1,19 @@
-
 #[test]
 fn test() {
     assert_eq!(2 + 3, 5);
 }
 
+/// https://practice.course.rs/variables.html
 #[test]
-fn test1() {
+fn test31() {
     let x: i32 = 5; // Uninitialized but used, ERROR !
     let _y: i32; // Uninitialized but also unused, only a Warning !
-
     assert_eq!(x, 5);
     println!("Success!");
 }
 
 #[test]
-fn test2() {
+fn test32() {
     let mut x = 1;
     x += 2;
     assert_eq!(x, 3);
@@ -22,7 +21,7 @@ fn test2() {
 }
 
 #[test]
-fn test3() {
+fn test33() {
     let x: i32 = 10;
     let y: i32 = 5;
     {
@@ -32,7 +31,7 @@ fn test3() {
 }
 
 #[test]
-fn test4() {
+fn test34() {
     define_x();
 }
 fn define_x() {
@@ -41,7 +40,7 @@ fn define_x() {
 }
 
 #[test]
-fn test5() {
+fn test35() {
     let x: i32 = 5;
     {
         let x = 12;
@@ -54,7 +53,7 @@ fn test5() {
 
 // Remove a line in the code to make it compile
 #[test]
-fn test6() {
+fn test36() {
     let mut x: i32 = 1; //warning: variable `x` is assigned to, but never used
     x = 7;
     x += 3; //warning: value assigned to `x` is never read
@@ -65,12 +64,12 @@ fn test6() {
 }
 
 #[test]
-fn test7() {
+fn test37() {
     let _x = 1;
 }
 
 #[test]
-fn test8() {
+fn test38() {
     let (mut x, y) = (1, 2);
     x += 2;
     assert_eq!(x, 3);
@@ -79,7 +78,7 @@ fn test8() {
 }
 
 #[test]
-fn test9() {
+fn test39() {
     let (x, y);
     (x, _) = (3, 4); //працює, але показує як помилка, замінити ..?
     [_, y] = [1, 2];
