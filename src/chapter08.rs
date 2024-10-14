2c8496c36064ce5b788420e2db87e60d4307afb7
Emptiness31 <levinaanna60@gmail.com>
2024-10-14 10:51:25 +0300

    chapter08.rs

--- src/chapter08.rs
+++ src/chapter08.rs
@@ -185,5 +185,56 @@ fn test811() {
 
 #[test]
 fn test812() {
+    // Fix the errors
+    enum Message {
+        Hello { id: i32 },
+    }
+    let msg = Message::Hello { id: 5 };
+    match msg {
+        Message::Hello {
+            id: id @ 3..=7,
+        } => println!("Found an id in range [3, 7]: {}", id),
+        Message::Hello { id: newid @ (10 | 11 | 12),
+        } => {
+            println!("Found an id in another range [10, 12]: {}", newid)
+        }
+        Message::Hello { id } => println!("Found some other id: {}", id),
+    }
+}
 
+#[test]
+// Fill in the blank to make the code work, `split` MUST be used
+fn test813() {
+    let num = Some(4);
+    let split = 5;
+    match num {
+        Some(x) if x < split => assert!(x < split),
+        Some(x) => assert!(x >= split),
+        None => (),
+    }
+    println!("Success!");
+}
+
+#[test]
+fn test814() {
+    // Fill the blank to make the code work
+    let numbers = (2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048);
+    match numbers {
+        (first, .., last) => {
+            assert_eq!(first, 2);
+            assert_eq!(last, 2048);
+        }
+    }
+    println!("Success!");
+}
+
+#[test]
+fn test815() {
+    // FIX the error with least changing
+    // DON'T remove any code line
+    let mut v = String::from("hello,");
+    let r = &mut v;
+    match r {
+        value => value.push_str(" world!")
+    }
 }
